daf8cc4cf2d75998b3a67a477ba30c5b10790ade
Marina Tislenko <26051978m@gmail.com>
2022-06-25 11:48:32 +0300

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         int amount = 100; //сумма на счете
-        int popol = 1100; // сумма пополнения
+        int refill = 1100; // сумма пополнения
         int rubles = 1; //1 рубль за каждые 100 рублей пополнения
-        if (popol > 1_000) {
+        if (refill > 1_000) {
             System.out.println("Начислены бонусы");
-            System.out.println(popol / 100);
+            System.out.println(refill / 100);
             System.out.println("Итоговый баланс");
-            System.out.println(popol + amount + popol / 100);
+            System.out.println(refill + amount + refill / 100);
         } else {
             System.out.println("Не начислены бонусы");
             System.out.println("Итоговый баланс");
-            System.out.println(popol + amount);
+            System.out.println(refill + amount);
         }
     }
 }
